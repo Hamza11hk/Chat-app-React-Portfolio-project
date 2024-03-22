c2551f1c12357f7832a211fa1d7fd6ff8d013863
Hamza11hk <Hamzahk110077@gmail.com>
2024-03-22 08:45:36 +0000

    Defining the google signin function & signout function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import { useState } from 'react'
-
 import './App.css'
+import { auth, db } from './firebase'
+import { useAuthsate } from 'react-firebase-hooks'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { addDoc, collection } from 'firebase/firestore'
 
 function App() {
+  const [user] = useAuthsate(auth)
+  const [currentRoom, setCurrentRoom] = useState('')
   
+  const googleSignIn = async() => {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider);
+    const { uid, displayName, photoURL } = await auth.currentUser;
+    await addDoc(collection(db, "Users") {
+      name: displayName,
+      avatar: photoURL,
+      createdAt: Date.now(),
+      uid
+    });
+  }
+
+  const signOut = () => {
+    auth.signOut()
+  }
+
+
 
   return (
     <div className='text-xl text-center'>
