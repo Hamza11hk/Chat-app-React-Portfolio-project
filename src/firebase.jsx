444c04e51f111bfcf46c5ca03d554d963381a323
Hamza11hk <Hamzahk110077@gmail.com>
2024-03-22 08:15:09 +0000

    setup Firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_KEY,
+  authDomain: "chat-app-react-project.firebaseapp.com",
+  projectId: "chat-app-react-project",
+  storageBucket: "chat-app-react-project.appspot.com",
+  messagingSenderId: import.meta.env.VITE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
