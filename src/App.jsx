7acabf0bd61c49274ae5bb362469167b2822cdc8
Hamza11hk <Hamzahk110077@gmail.com>
2024-03-24 07:40:13 +0000

    Improve UI and authentication functionality

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,35 @@ function App() {
 
 
   return (
-    <div className='text-xl text-center'>
-      Hello chat application
+    <div className='text-xl w-full h-screen px-[5%] text-center font-semibold flex flex-col'>
+      <div className='flex justify-between w-full h-auto items-center'>
+        <h1 className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-pink-900'>Chat App</h1>
+        {
+          user && <h2>Welcome {user.displayName}</h2>
+        }
+
+        {
+          user ? (
+            <button onClick={signOut} className='border-black border-2 rounded-xl font-bold p-4 text-center w-[13rem]'>
+              signOut
+            </button>
+          ) : (
+            <button onClick={googleSignIn} className='border-black border-2 rounded-xl font-bold p-4 text-center w-[13rem]'>
+              Google Sign In
+            </button>
+          )
+        }
+      
+      </div>
+
+      {
+        !user && <div className='w-full h-[25rem] flex justify-center items-center font-bold text-5xl text-transparent bg-clip-text bg-gradient-to-t from-purple-600 to-red-800'>
+
+          Welcome to Chat Room, Sign In Now.
+          
+        </div>
+      }
+
     </div>
   )
 }
